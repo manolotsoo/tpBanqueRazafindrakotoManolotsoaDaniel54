fa7d8d5f00df831116988260372a0f6fe80a8715
Manolotsoa <manolotsoadaniel@gmail.com>
2024-04-18 14:54:33 +0300

    Modifiez l'entité CompteBancaire

--- src/main/java/mg/manolotsoa/tpbanquerazafindrakotomanolotsoadaniel54/entity/CompteBancaire.java
+++ src/main/java/mg/manolotsoa/tpbanquerazafindrakotomanolotsoadaniel54/entity/CompteBancaire.java
@@ -4,14 +4,19 @@
  */
 package mg.manolotsoa.tpbanquerazafindrakotomanolotsoadaniel54.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.xml.bind.annotation.XmlRootElement;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -36,16 +41,25 @@ public class CompteBancaire implements Serializable {
     @Column(name = "SOLDE")
     int solde;
 
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<OperationBancaire> operations = new ArrayList<>();
+
     public CompteBancaire() {
+    }
+
+    public List<OperationBancaire> getOperations() {
+        return operations;
     }
 
     public CompteBancaire(String nom, int solde) {
         this.nom = nom;
         this.solde = solde;
+        this.operations.add(new OperationBancaire("Création du compte", solde));
     }
 
     public void deposer(int montant) {
         solde += montant;
+        this.operations.add(new OperationBancaire("Débit", solde));
     }
 
     public void retirer(int montant) {
@@ -54,6 +68,7 @@ public class CompteBancaire implements Serializable {
         } else {
             solde = 0;
         }
+        this.operations.add(new OperationBancaire("Crédit", solde));
     }
 
     public void setNom(String nom) {
---
+++ src/main/java/mg/manolotsoa/tpbanquerazafindrakotomanolotsoadaniel54/entity/OperationBancaire.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.manolotsoa.tpbanquerazafindrakotomanolotsoadaniel54.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author manou
+ */
+@Entity
+public class OperationBancaire implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private LocalDateTime dateOperation;
+    private int montant;
+
+    public OperationBancaire() {
+    }
+
+    public OperationBancaire(String description, int montant) {
+        this.description = description;
+        this.montant = montant;
+        dateOperation = LocalDateTime.now();
+    }
+
+}
